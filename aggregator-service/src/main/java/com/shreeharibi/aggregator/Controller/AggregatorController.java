51f6b825c822354a2ac01b8270682549fd6a2811
Shree Hari Bittugondanahalli Indra Kumar <shreehariuvce@gmail.com>
2022-01-11 17:21:45 +0100

    Implement add new comment workflow
    Use comment-service grpc stub to make call

--- aggregator-service/src/main/java/com/shreeharibi/aggregator/Controller/AggregatorController.java
+++ aggregator-service/src/main/java/com/shreeharibi/aggregator/Controller/AggregatorController.java
@@ -1,5 +1,6 @@
 package com.shreeharibi.aggregator.Controller;
 
+import com.shreeharibi.aggregator.Model.Comment;
 import com.shreeharibi.aggregator.Model.Note;
 import com.shreeharibi.aggregator.Service.AggregatorService;
 import lombok.extern.slf4j.Slf4j;
@@ -26,12 +27,19 @@ public class AggregatorController {
         return ResponseEntity.ok().body(true);
     }
 
-    @PostMapping("add")
+    @PostMapping("add-note")
     public ResponseEntity<Boolean> addNewNote(
         @RequestBody Note note
     ) {
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/add").toUriString());
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/add-note").toUriString());
         return ResponseEntity.created(uri).body(service.createNote(note));
     }
 
+    @PostMapping("add-comment")
+    public ResponseEntity<Boolean> addNewCommentToNote(
+            @RequestBody Comment comment
+    ) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/add-comment").toUriString());
+        return ResponseEntity.created(uri).body(service.addNewCommentToNote(comment));
+    }
 }
--- aggregator-service/src/main/java/com/shreeharibi/aggregator/Service/AggregatorService.java
+++ aggregator-service/src/main/java/com/shreeharibi/aggregator/Service/AggregatorService.java
@@ -1,6 +1,11 @@
 package com.shreeharibi.aggregator.Service;
 
+import com.shreeharibi.aggregator.Model.Comment;
 import com.shreeharibi.aggregator.Model.Note;
+import com.shreeharibi.notemaker.comments.AddNewCommentRequest;
+import com.shreeharibi.notemaker.comments.AddNewCommentResponse;
+import com.shreeharibi.notemaker.comments.CommentDto;
+import com.shreeharibi.notemaker.comments.CommentsServiceGrpc;
 import com.shreeharibi.notemaker.notes.NoteCreationRequest;
 import com.shreeharibi.notemaker.notes.NoteCreationResponse;
 import com.shreeharibi.notemaker.notes.NoteServiceGrpc;
@@ -15,17 +20,44 @@ public class AggregatorService {
     @GrpcClient("notes-service")
     private NoteServiceGrpc.NoteServiceBlockingStub noteStub;
 
+    @GrpcClient("comments-service")
+    private CommentsServiceGrpc.CommentsServiceBlockingStub commentStub;
+
     public Boolean createNote(Note note) {
-        NoteCreationRequest noteCreationRequest = NoteCreationRequest.newBuilder()
-                                                .setTitle(note.getTitle())
-                                                .setNote(note.getNote())
-                                                .build();
         try {
+            NoteCreationRequest noteCreationRequest = NoteCreationRequest.newBuilder()
+                    .setTitle(note.getTitle())
+                    .setNote(note.getNote())
+                    .build();
             log.info("Invoking notes-microservice...");
             NoteCreationResponse response = noteStub.createNote(noteCreationRequest);
             return response.getResponse();
         } catch (Exception exception) {
             log.error("Failed to create note");
+        }
+        return false;
+    }
+
+    public Boolean addNewCommentToNote(Comment comment) {
+        try {
+            log.info("Invoking comments-microservice...");
+
+            // Refer proto file for structure
+            CommentDto commentDto = CommentDto.newBuilder()
+                    .setComment(comment.getComment())
+                    .setNoteId(comment.getNoteId())
+                    .build();
+            AddNewCommentRequest commentRequest = AddNewCommentRequest.newBuilder()
+                    .setComment(commentDto)
+                    .build();
+            AddNewCommentResponse addNewCommentResponse = commentStub.addComment(commentRequest);
+            if (addNewCommentResponse != null) {
+                return addNewCommentResponse.getStatus();
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            log.error("Falied to add comment...");
         }
         return false;
     }
